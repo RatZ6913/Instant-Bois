03d1054a9f7f9a6a20d3dc23cfedb6b3632da9f4
Pheak Rattana <rattana.pheak.rp@gmail.com>
2022-12-23 17:59:46 +0100

    Carousel Parti : 1
    Carousel Parti : 1

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 let boxCarousel = document.querySelector('#carousel');
 let imgCarousel = document.createElement('div');
 imgCarousel.classList.add('imgCarousel');
+let chantier1 = document.querySelector('#arr1');
+let chantier2 = document.querySelector('#arr2');
+let chantier3 = document.querySelector('#arr3');
+let chantier4 = document.querySelector('#arr4');
 let next = document.querySelector('#next');
 
 let arrChantier1 = [
@@ -12,23 +16,82 @@ let arrChantier1 = [
   "url(/public/img/chantier1_6.jpg)"
 ];
 
-imgCarousel.style.backgroundImage = arrChantier1[0];
+let arrChantier2 = [
+  "url(/public/img/chantier2_1.jpg)",
+  "url(/public/img/chantier2_2.jpg)",
+  "url(/public/img/chantier2_3.jpg)",
+  "url(/public/img/chantier2_4.jpg)",
+  "url(/public/img/chantier2_5.jpg)"
+];
+
+let arrChantier3 = [
+  "url(/public/img/chantier3_1.jpg)",
+  "url(/public/img/chantier3_2.jpg)",
+  "url(/public/img/chantier3_3.jpg)",
+  "url(/public/img/chantier3_4.jpg)",
+  "url(/public/img/chantier3_5.jpg)"
+];
+
+let arrChantier4 = [
+  "url(/public/img/chantier4_1.jpg)",
+  "url(/public/img/chantier4_2.jpg)",
+  "url(/public/img/chantier4_3.jpg)",
+  "url(/public/img/chantier4_4.jpg)",
+  "url(/public/img/chantier4_5.jpg)",
+  "url(/public/img/chantier4_6.jpg)",
+  "url(/public/img/chantier4_7.jpg)",
+  "url(/public/img/chantier4_8.jpg)"
+];
+
 next.before(imgCarousel);
-let i = 0;
 
+chantier1.addEventListener('click', () => {
+  let i = 0;
+  imgCarousel.style.backgroundImage = arrChantier1[0];
+  next.addEventListener('click', () => {
+    i++;
+    if (i == arrChantier1.length) {
+      i = 0;
+    }
+    imgCarousel.style.backgroundImage = arrChantier1[i];
+  });
+})
 
-next.addEventListener('click', () => {
-  imgCarousel.style.transform = "translate( 50%)";
-  imgCarousel.style.transition = "2s ease-in-out";
-  i++;
+chantier2.addEventListener('click', () => {
+  let j = 0;
+  imgCarousel.style.backgroundImage = arrChantier2[0];
+  next.addEventListener('click', () => {
+    j++;
+    if (j == arrChantier2.length) {
+      j = 0;
+    }
+    imgCarousel.style.backgroundImage = arrChantier2[j];
+  });
+})
 
-  if (i == arrChantier1.length) {
-    i = 0;
-  }
-  imgCarousel.style.backgroundImage = arrChantier1[i];
+chantier3.addEventListener('click', () => {
+  let k = 0;
+  imgCarousel.style.backgroundImage = arrChantier3[0];
+  next.addEventListener('click', () => {
+    k++;
+    if (k == arrChantier3.length) {
+      k = 0;
+    }
+    imgCarousel.style.backgroundImage = arrChantier3[k];
+  });
+})
 
-});
-
+chantier4.addEventListener('click', () => {
+  let l = 0;
+  imgCarousel.style.backgroundImage = arrChantier4[0];
+  next.addEventListener('click', () => {
+    l++;
+    if (l == arrChantier4.length) {
+      l = 0;
+    }
+    imgCarousel.style.backgroundImage = arrChantier4[l];
+  });
+})
 
 
 

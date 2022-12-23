4b5dd39630fc4b2a18a48b236ce3538865aee923
Pheak Rattana <rattana.pheak.rp@gmail.com>
2022-12-23 17:02:20 +0100

    Update
    Update

--- public/script.js
+++ public/script.js
@@ -1,2 +1,32 @@
 let boxCarousel = document.querySelector('#carousel');
+let imgCarousel = document.createElement('div');
+imgCarousel.classList.add('imgCarousel');
+let next = document.querySelector('#next');
+
+let arrChantier1 = [
+  "url(/public/img/chantier1_1.jpg)",
+  "url(/public/img/chantier1_2.jpg)",
+  "url(/public/img/chantier1_3.jpg)",
+  "url(/public/img/chantier1_4.jpg)",
+  "url(/public/img/chantier1_5.jpg)",
+  "url(/public/img/chantier1_6.jpg)"
+];
+
+imgCarousel.style.backgroundImage = arrChantier1[0];
+next.before(imgCarousel);
+let i = 0;
+
+
+next.addEventListener('click', () => {
+  imgCarousel.style.backgroundImage = arrChantier1[i];
+  i++;
+
+  if (i == arrChantier1.length) {
+    i = 0;
+  }
+});
+
+
+
+
 

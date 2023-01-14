ba2d406531bcc4eb87f508f48b5d705d32c24617
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-01-14 17:57:00 +0100

    Update Delete Old Code
    Update Delete Old Code

--- public/script.js
+++ public/script.js
@@ -50,9 +50,7 @@ activityBox.forEach((element, index) => {
   });
 });
 
-
 // PARTIE : PHOTOS ( CARROUSEL )
-
 let btnPrevious = document.querySelectorAll('.previous');
 let btnNext = document.querySelectorAll('.next');
 let imgSite = document.querySelectorAll('.img-site .imgSite');
@@ -89,13 +87,11 @@ let arrImgSite = [
 ];
 
 imgSite.forEach((element, index) => {
-
   if (index == index) {
     let countSite = 0;
 
     btnNext[index].addEventListener('click', () => {
       countSite++;
-      
       if (countSite < arrImgSite[index].length) {
         element.src = arrImgSite[index][countSite];
       } else {
@@ -104,7 +100,7 @@ imgSite.forEach((element, index) => {
       }
     });
 
-    btnPrevious[index].addEventListener('click', (e) => {
+    btnPrevious[index].addEventListener('click', () => {
       countSite--;
       if (countSite < 0) {
         countSite = arrImgSite[index].length - 1;
@@ -116,217 +112,3 @@ imgSite.forEach((element, index) => {
 
 
 
-
-
-// ///Selection de la div principale du carrousel///
-// const activeImg = document.querySelector("#activeImg"),
-//   img = document.querySelector("#activeImg img"),
-//   buttonLeft = document.querySelector('.left'),
-//   buttonRight = document.querySelector('.right');
-
-// ///bouton choix du carrousel///
-// const sites = document.querySelectorAll('.site')
-
-// ///déclaration des tableaux contenant les images des chantiers///
-// const tabSites = [];
-// for (let site = 0; site < 4; site++) {
-//   tabSites.push([]);
-// }
-
-// ///ajout des photos dans chaque tableau///
-
-// var sitesData = [
-//   { id: 1, limit: 7 },
-//   { id: 2, limit: 6 },
-//   { id: 3, limit: 6 },
-//   { id: 4, limit: 9 }
-// ];
-
-// for ({ id, limit } of sitesData) {
-
-//   for (let numImg = 1; numImg < limit; numImg++) {
-//     tabSites[id - 1].push(`./public/img/chantier${id}_${numImg}.jpg`);
-//   }
-
-// }
-
-// let imgCount = 0;
-
-// ///Bouton de choix de chantier///
-// for (let i = 0; i < 4; i++) {
-
-//   sites[i].addEventListener('click', () => {
-//     imgCount = 0;
-//     img.setAttribute('data-id', `site${i + 1}`);
-//     img.setAttribute('alt', `chantier ${i + 1}`);
-//     img.classList.add('fadeIn');
-//     setTimeout(() => img.src = tabSites[i][0], 200);
-//     img.addEventListener('transitionend', () => {
-
-//       img.classList.remove('fadeIn');
-//     })
-
-
-//   })
-
-// }
-
-// ///boutons du carrousel///
-// buttonRight.addEventListener('click', () => {
-
-//   ///désactivation du bouton///
-//   buttonRight.disabled = "true";
-
-//   imgCount++;
-
-//   switch (img.dataset.id) {
-//     case "site1":
-//       if (imgCount > tabSites[0].length - 1) {
-//         imgCount = 0;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[0][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-
-//         ///activation du bouton///
-//         buttonRight.removeAttribute("disabled", "")
-
-//       })
-
-//       break;
-
-//     case 'site2':
-//       if (imgCount > tabSites[1].length - 1) {
-//         imgCount = 0;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[1][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonRight.removeAttribute("disabled", "")
-//       })
-//       break;
-
-//     case 'site3':
-//       if (imgCount > tabSites[2].length - 1) {
-//         imgCount = 0;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[2][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonRight.removeAttribute("disabled", "")
-//       })
-//       break;
-
-//     case 'site4':
-//       if (imgCount > tabSites[3].length - 1) {
-//         imgCount = 0;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[3][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonRight.removeAttribute("disabled", "")
-//       })
-//       break;
-//   }
-//   console.log(imgCount);
-// })
-
-// buttonLeft.addEventListener('click', () => {
-
-//   buttonLeft.disabled = "true";
-
-//   imgCount--;
-
-//   switch (img.dataset.id) {
-//     case "site1":
-//       if (imgCount < 0) {
-//         imgCount = tabSites[0].length - 1;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[0][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonLeft.removeAttribute("disabled", "")
-//       })
-
-//       break;
-
-//     case 'site2':
-//       if (imgCount < 0) {
-//         imgCount = tabSites[1].length - 1;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[1][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonLeft.removeAttribute("disabled", "")
-//       })
-
-//       break;
-
-//     case 'site3':
-//       if (imgCount < 0) {
-//         imgCount = tabSites[2].length - 1;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[2][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonLeft.removeAttribute("disabled", "")
-//       })
-
-//       break;
-
-//     case 'site4':
-//       if (imgCount < 0) {
-//         imgCount = tabSites[3].length - 1;
-//       }
-
-//       img.classList.add('fadeIn');
-//       setTimeout(() => img.src = tabSites[3][imgCount], 200)
-
-//       img.addEventListener('transitionend', () => {
-
-//         img.classList.remove('fadeIn');
-//         buttonLeft.removeAttribute("disabled", "")
-//       })
-//       next.before(imgCarousel);
-
-//       imgAnim = (param) => {
-//         param.style.transition = "transform 0.1s ease";
-//         param.style.transform = "scaleY(0.9)";
-
-//         setTimeout(() => {
-//           param.style.transform = "scaleY(1)";
-//         }, 90);
-//       };
-
-//       break;
-//   }
-
-// })
